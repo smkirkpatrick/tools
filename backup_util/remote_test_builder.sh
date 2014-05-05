b4d8b6b2f0abb5a938a8fc8b4d91cc890b46ac7b
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2014-05-05 09:20:35 -0400

    script now builds remote test shell script

--- backup_util/remote_test_builder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "#!/bin/sh" > remote_test_pass11.sh
-cat backup_movs_pass11.log | grep "confirmed duplicate of remote" >> remote_test_pass11.sh
-sed -i -e "s/^.*remote server file \[\(.*\)\]\. Can remove.*/echo \"\1\"; open \"\1\"; sleep 7;/" remote_test_pass11.sh 
-chmod +x remote_test_pass11.sh
