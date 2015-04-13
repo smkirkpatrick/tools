723663d106561a7fda5d330c551ff6e9a22f6b11
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2015-04-13 13:02:31 -0400

    added archiving of prior run result scripts, generalized variable naming

--- backup_util/backup.py
+++ backup_util/backup.py
@@ -11,23 +11,26 @@ import subprocess
 import shutil
 
 local_cleanup_file = None
+local_cleanup_file_name = 'local_cleanup.sh'
+
 remote_test_file = None
+remote_test_file_name = 'remote_test.sh'
 
 def add_file_to_cleanup(file):
 	global local_cleanup_file
 	if local_cleanup_file is None:
-		local_cleanup_file = open('local_cleanup.sh','wx')
+		local_cleanup_file = open(local_cleanup_file_name,'wx')
 		local_cleanup_file.write('#!/bin/sh\n')
 	local_cleanup_file.write("rm \"{}\";\n".format(file))
 
 def add_remote_file_to_test(file):
 	global remote_test_file
 	if remote_test_file is None:
-		remote_test_file = open('remote_test.sh','wx')
+		remote_test_file = open(remote_test_file_name,'wx')
 		remote_test_file.write('#!/bin/sh\n')
 	remote_test_file.write("echo \"{}\";\nopen \"{}\";\nsleep 10;\n".format(file,file))
 
-def check_for_remote_duplicates(movie, movie_file, local_year, optimize_search):
+def check_for_remote_duplicates(media_file, media_filename, local_year, optimize_search):
 	server_dup = None
 
 	server_search_path = server_remote_base
@@ -37,8 +40,8 @@ def check_for_remote_duplicates(movie, movie_file, local_year, optimize_search):
 	# ^ local_year path extension is an optimization that can cause issues if the local files were copied over in 2013 but created in 2011, for example. The original files in 2011 on the server won't be identified as duplicates because we'd only be checking the 2013 remove path
 
 	try:
-		print "check remote backup for [{}] in [{}]...".format(movie, server_search_path)
-		server_dups = subprocess.check_output(["find", server_search_path, "-name", movie_file, "-print"])
+		print "check remote backup for [{}] in [{}]...".format(media_file, server_search_path)
+		server_dups = subprocess.check_output(["find", server_search_path, "-name", media_filename, "-print"])
 		dups = filter(bool, server_dups.split('\n'))
 	except CalledProcessError:
 		# print "Find exception: [{}]".format(sys.exc_info()[0])
@@ -46,9 +49,9 @@ def check_for_remote_duplicates(movie, movie_file, local_year, optimize_search):
 
 	sha_duplicate = False
 	if dups and len(dups) > 0:
-		print "[{}] server dup(s) found for [{}]: [{}]".format(len(dups), movie_file, dups)
+		print "[{}] server dup(s) found for [{}]: [{}]".format(len(dups), media_filename, dups)
 
-		local_sha_pipe = subprocess.Popen(['openssl', 'sha1', movie], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+		local_sha_pipe = subprocess.Popen(['openssl', 'sha1', media_file], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 		local_sha_out, err = local_sha_pipe.communicate()
 		local_sha_output_array = local_sha_out.split()
 		local_sha = local_sha_output_array[len(local_sha_output_array)-1]
@@ -65,8 +68,8 @@ def check_for_remote_duplicates(movie, movie_file, local_year, optimize_search):
 			print "remote_sha: [{}] for [{}]".format(remote_sha, dup)
 			
 			if local_sha == remote_sha:
-				print "[{}] sha checksum the same [{}], confirmed duplicate of remote server file [{}]. Can remove local copy [{}].".format(movie_file, local_sha, dup, movie)
-				add_file_to_cleanup(movie)
+				print "[{}] sha checksum the same [{}], confirmed duplicate of remote server file [{}]. Can remove local copy [{}].".format(media_filename, local_sha, dup, media_file)
+				add_file_to_cleanup(media_file)
 				add_remote_file_to_test(dup)
 				sha_duplicate = True
 				break
@@ -76,27 +79,42 @@ def check_for_remote_duplicates(movie, movie_file, local_year, optimize_search):
 def get_creation_time(path):
 	sub_output_array = subprocess.check_output(["stat", "-f%B", path])
 	create_time_in_secs = filter(bool, sub_output_array.split('\n'))
-	# print "get_creation_time: [{}]".format(str(create_time_in_secs))
+	# print "get_creation_time: [{}]".format(str(datetime.datetime.fromtimestamp(float(create_time_in_secs[0]))))
 	return datetime.datetime.fromtimestamp(float(create_time_in_secs[0]))
+
+def archive_file(file):
+	create_time = get_creation_time(file)
+	src = file;
+	dst = "archive/{:0>4d}{:0>2d}{:0>2d}_{:0>2d}{:0>2d}{:0>2d}-{}".format(create_time.year, create_time.month, create_time.day, create_time.hour, create_time.minute, create_time.second, file)
+	shutil.move(src, dst)
+	print "Archived {}".format(dst)
+
+def archive_previous_run_results():
+	if os.path.isfile(local_cleanup_file_name):
+		archive_file(local_cleanup_file_name)
+	if os.path.isfile(remote_test_file_name):
+		archive_file(remote_test_file_name)
 
 arg_check_dups = True
 arg_backup_type = ".MOV"
 
-print 'Argument List:', str(sys.argv)
+archive_previous_run_results()
+
+# print 'Argument List:', str(sys.argv)
 
 try:
 	opts, args = getopt.getopt(sys.argv[1:], "ht:", ["help", "type=", "skip-dup-check"])
 except getopt.GetoptError:
-	print 'backup_movs.py [-h] -t <movies|pics|screenshots> [--skip-dup-check]'
+	print 'backup.py [-h] -t <movies|pics|screenshots> [--skip-dup-check]'
 	sys.exit(2)
 
-print "getopt opts: [{}]".format(str(opts))
-print "getopt args: [{}]".format(str(args))
+# print "getopt opts: [{}]".format(str(opts))
+# print "getopt args: [{}]".format(str(args))
 
 for opt, arg in opts:
-	print "Checking opt: [{}] arg: [{}]".format(opt, arg)
+	# print "Checking opt: [{}] arg: [{}]".format(opt, arg)
 	if opt in ("-h", "--help"):
-		print 'backup_movs.py [-h] -t <movies|pics|screenshots> [--skip-dup-check]'
+		print 'backup.py [-h] -t <movies|pics|screenshots> [--skip-dup-check]'
 		sys.exit()
 	elif opt in ("-t", "--type"):
 		if arg == 'movies':
@@ -111,41 +129,39 @@ for opt, arg in opts:
 		arg_check_dups = False
 
 local_root_path = '/Users/seanmkirkpatrick/Pictures/iPhoto Library.photolibrary/Masters'
-found_movies = subprocess.check_output(["find", local_root_path, "-name", "*{}".format(arg_backup_type), "-print"])
+found_media = subprocess.check_output(["find", local_root_path, "-name", "*{}".format(arg_backup_type), "-print"])
 print "Ran check_output"
 
-movies = filter(bool, found_movies.split('\n'))
-movie_info = {}
-for movie in movies:
-	movie_info[movie] = movie.split('/')
-	print "local movie file path: [{}]".format(movie)
+media = filter(bool, found_media.split('\n'))
+media_file_info = {}
+for media_file in media:
+	media_file_info[media_file] = media_file.split('/')
+	print "local media_file file path: [{}]".format(media_file)
 
-# for movie in movies:
-# 	print "[{}] => [{}]".format(movie, movie_info[movie])
-# 	print "[{}] => [{}][{}]".format(movie, movie_info[movie][1], movie_info[movie][len(movie_info[movie])-1])
+# for media_file in media:
+# 	print "[{}] => [{}]".format(media_file, media_file_info[media_file])
+# 	print "[{}] => [{}][{}]".format(media_file, media_file_info[media_file][1], media_file_info[media_file][len(media_file_info[media_file])-1])
 
 server_remote_base = '/Volumes/pictures/MASTER PICTURES'
-for movie in movies:
-	print "Processing [{}]".format(movie)
+for media_file in media:
+	print "Processing [{}]".format(media_file)
 
-	movie_create_time = get_creation_time(movie)
-	year = movie_create_time.year
-	movie_file = movie_info[movie][len(movie_info[movie])-1]
-	print "[{}] file [{}] year: [{}]".format(movie, movie_file, year)
+	media_file_create_time = get_creation_time(media_file)
+	year = media_file_create_time.year
+	media_filename = media_file_info[media_file][len(media_file_info[media_file])-1]
+	print "[{}] file [{}] created: [{}/{}/{}]".format(media_file, media_filename, media_file_create_time.month, media_file_create_time.day, year)
 
 	sys.stdout.flush()
 
-	if arg_check_dups and check_for_remote_duplicates(movie, movie_file, str(year), year >= 2013):
+	if arg_check_dups and check_for_remote_duplicates(media_file, media_filename, str(year), year >= 2013):
 		# We're done with this file, move on
 		continue
 
-	remote_dir = server_remote_base + '/' + movie_create_time.strftime("%Y/%Y%m/%Y%m%d/")
-	remote_movie = remote_dir + movie_file
+	remote_dir = server_remote_base + '/' + media_file_create_time.strftime("%Y/%Y%m/%Y%m%d/")
+	remote_media_file = remote_dir + media_filename
 
-	print "[{}] only found locally, backing up to server: [{}]".format(movie, remote_movie)
+	print "[{}] only found locally, backing up to server: [{}]".format(media_file, remote_media_file)
 	sys.stdout.flush()
-
-	continue
 
 	try:
 		os.makedirs(remote_dir, 0o755)
@@ -155,7 +171,7 @@ for movie in movies:
 		else:
 			raise
 
-	shutil.copyfile(movie, remote_movie)
+	shutil.copyfile(media_file, remote_media_file)
 
 if local_cleanup_file is not None:
 	local_cleanup_file.close()
